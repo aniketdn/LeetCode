f7f39c39ba1b734a9dae3df6c212c8f1c0cfab07
Aniket Nandanwar <aniketdnandanwar@gmail.com>
2019-09-12 09:42:50 -0400

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,7 +1,19 @@
-class Solution:
+class Solution(object):
     def twoSum(self, nums, target):
-        lookup={}
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l=[]
         for i in range(0,len(nums)):
-            if(target-nums[i] in lookup):
-                return [lookup[target-nums[i]],i]
-            lookup[nums[i]]=i
+            rem=target-nums[i]
+            print rem
+            if rem in nums and nums.index(rem)!=i:
+                l.append(i)
+                l.append(nums.index(rem))
+                break
+        l.sort()
+        return l
+                
+        
